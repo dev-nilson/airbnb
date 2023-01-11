464a5aa64c756a4c2cf8e4c3de07add70aa7ca52
Denilson <dlemus@harding.edu>
2023-01-10 23:11:39 -0600

    ✨ Add image to banner

---
+++ components/Banner.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+function Banner() {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+      <Image
+        src="https://links.papareact.com/0fm"
+        alt="banner"
+        fill={true}
+        style={{ objectFit: "cover" }}
+      />
+    </div>
+  );
+}
+
+export default Banner;
