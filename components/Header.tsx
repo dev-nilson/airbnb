e31f29489a1c678df313481a7c7d9cad50943338
Denilson <dlemus@harding.edu>
2023-01-10 22:56:32 -0600

    ✨ Add search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <header>
-      <div className="relative flex items-center cursor-pointer">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center">
         <Image
           src="https://links.papareact.com/qd3"
           width={125}
@@ -11,7 +12,14 @@ function Header() {
           alt="logo"
         />
       </div>
-      <div></div>
+      <div className="flex items-center md:border-2 rounded-full py-2">
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
       <div></div>
     </header>
   );
