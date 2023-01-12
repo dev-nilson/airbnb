838b3b4b0ad015997d8c1fe870175c8f7d5bdeca
Denilson <dlemus@harding.edu>
2023-01-11 18:48:52 -0600

    ✨ Fetch places data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
+import { Place } from "../typescript/interfaces";
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 
-const Home: NextPage = () => {
+type HomeProps = {
+  exploreData: Place[];
+};
+
+const Home: NextPage<HomeProps> = ({ exploreData }) => {
   return (
     <div className="">
       <Head>
@@ -17,11 +22,22 @@ const Home: NextPage = () => {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-3xl font-semibold pb-5">Explore Nearby</h2>
-          
         </section>
       </main>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
+}
 
 export default Home;
