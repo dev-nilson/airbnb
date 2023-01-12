72ce936145e3bee7237029615f64f51cc023415e
Denilson <dlemus@harding.edu>
2023-01-11 19:04:36 -0600

    ✨ Add small card components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Place } from "../typescript/interfaces";
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
 
 type HomeProps = {
   exploreData: Place[];
@@ -22,6 +23,16 @@ const Home: NextPage<HomeProps> = ({ exploreData }) => {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-3xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.map((item) => (
+              <SmallCard
+                key={item.img}
+                image={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
