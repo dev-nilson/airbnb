29d02d3f1a89d77533a1598ce0ca71fa0b681704
Denilson <dlemus@harding.edu>
2023-01-11 19:12:45 -0600

    🚚 Rename home props and interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from "next";
-import { Place } from "../typescript/interfaces";
+import { City } from "../typescript/interfaces";
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
 
 type HomeProps = {
-  exploreData: Place[];
+  primaryData: City[];
+  secondaryData: City[];
 };
 
-const Home: NextPage<HomeProps> = ({ exploreData }) => {
+const Home: NextPage<HomeProps> = ({ primaryData, secondaryData }) => {
   return (
     <div className="">
       <Head>
@@ -24,7 +25,7 @@ const Home: NextPage<HomeProps> = ({ exploreData }) => {
         <section className="pt-6">
           <h2 className="text-3xl font-semibold pb-5">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData.map((item) => (
+            {primaryData.map((item) => (
               <SmallCard
                 key={item.img}
                 image={item.img}
@@ -44,13 +45,14 @@ const Home: NextPage<HomeProps> = ({ exploreData }) => {
 };
 
 export async function getStaticProps() {
-  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+  const primaryData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
     (res) => res.json()
   );
 
+
   return {
     props: {
-      exploreData,
+      primaryData,
     },
   };
 }
--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -1,4 +1,4 @@
-export interface Place {
+export interface City {
   img: string;
   location: string;
   distance: string;
