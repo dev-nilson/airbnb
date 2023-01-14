65cea5be248a10b08a017c834fd71fe5be52ef51
Denilson <dlemus@harding.edu>
2023-01-14 02:05:21 -0600

    🏷️ Add type for search results

--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -8,3 +8,15 @@ export interface Place {
   img: string;
   title: string;
 }
+
+export interface Result {
+  img: string;
+  location: string;
+  title: string;
+  description: string;
+  star: number;
+  price: string;
+  total: string;
+  long: number;
+  lat: number;
+}
