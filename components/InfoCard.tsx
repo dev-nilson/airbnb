999230002367c1a8d0edefa986de76ded2e191ff
Denilson <dlemus@harding.edu>
2023-01-14 14:50:27 -0600

    ✨ Add info for search results

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Result } from "../typescript/interfaces";
 import { HeartIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
 
 function InfoCard({
   img,
@@ -20,6 +21,25 @@ function InfoCard({
           alt="result"
           style={{ objectFit: "cover" }}
         />
+      </div>
+      <div className="flex flex-col flex-grow pl-5">
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+        <h4 className="text-xl font-semibold">{title}</h4>
+        <div className="border-b w-10 pt-2"></div>
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+        <div className="flex justify-between items-end">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+          <div>
+            <p className="text-lg lg:text-2xl font-semibold pb-2">{price}</p>
+            <p className="text-right font-extralight">{total}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
