6013bfb1a550ed981854a7ad0c24b89bd40788bf
Denilson <dlemus@harding.edu>
2023-01-13 18:05:12 -0600

    ✨ Add search results title

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,10 +5,15 @@ function Search() {
   return (
     <div className="h-screen">
       <Header />
-
+      <main>
+        <section>
+          <p className="text-xs">300+ stays for 5 number of guests</p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in New York</h1>
+        </section>
+      </main>
       <Footer />
     </div>
-  )
+  );
 }
 
 export default Search;
