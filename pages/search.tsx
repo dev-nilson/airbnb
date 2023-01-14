aeeb149bd11b1f5785639b00c96baa63e35478b2
Denilson <dlemus@harding.edu>
2023-01-13 18:37:18 -0600

    ✨ Add dynamic search filters

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,15 +1,25 @@
+import { useRouter } from "next/router";
+import { format } from "date-fns";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Search() {
+  const router = useRouter();
+  const { location, startDate, endDate, guests } = router.query;
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yyyy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yyyy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
   return (
     <div className="h-screen">
       <Header />
       <main>
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">300+ stays for 5 number of guests</p>
+          <p className="text-xs">
+            {range} for {guests} guests
+          </p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">
-            Stays in New York
+            Stays in {location}
           </h1>
 
           <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
