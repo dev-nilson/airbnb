c5d4daf349665876c6adc5e6d1bf2bb7baa8bf33
Denilson <dlemus@harding.edu>
2023-01-13 18:16:59 -0600

    ✨ Add filters for search

--- pages/search.tsx
+++ pages/search.tsx
@@ -6,9 +6,19 @@ function Search() {
     <div className="h-screen">
       <Header />
       <main>
-        <section>
+        <section className="flex-grow pt-14 px-6">
           <p className="text-xs">300+ stays for 5 number of guests</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in New York</h1>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in New York
+          </h1>
+
+          <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="chip">Cancellation Flexibility</p>
+            <p className="chip">Type of Place</p>
+            <p className="chip">Price</p>
+            <p className="chip">Rooms and Beds</p>
+            <p className="chip">More Filters</p>
+          </div>
         </section>
       </main>
       <Footer />
