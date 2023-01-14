b1a1d454151877a451fbfbded3c4d6910bac2201
Denilson <dlemus@harding.edu>
2023-01-14 14:54:33 -0600

    💄 Style info cards

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -13,7 +13,7 @@ function InfoCard({
   total,
 }: Result) {
   return (
-    <div className="flex">
+    <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-300 ease-out first:border-t">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
         <Image
           src={img}
