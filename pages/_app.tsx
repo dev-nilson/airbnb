5ea02598b0475df4b5e36ce6e11700c9dc8ee89e
Denilson <dlemus@harding.edu>
2023-01-15 22:50:00 -0600

    ✨ Connect progress bar to router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
 import ProgressBar from "@badrap/bar-of-progress";
 import "../styles/globals.css";
 
@@ -8,6 +9,10 @@ const progress = new ProgressBar({
   className: "z-50",
   delay: 100,
 });
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
