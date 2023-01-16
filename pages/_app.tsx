15e0dfb39f6f56cc542ff0234a3288307944729b
Denilson <dlemus@harding.edu>
2023-01-15 22:44:41 -0600

    ✨ Create progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import ProgressBar from "@badrap/bar-of-progress";
+import "../styles/globals.css";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595E",
+  className: "z-50",
+  delay: 100,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
