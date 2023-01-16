ff14e35ae2c9d7e55ba8c89734627739c997a2af
Denilson <dlemus@harding.edu>
2023-01-16 17:41:18 -0600

    ✨ Make map dynamic

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import ReactMapGL from "react-map-gl";
 
 function Map() {
-  const [viewport, setViewport] = useState({
+  const [viewport, setViewport] = useState<any>({
     with: "100%",
     height: "100%",
     latitude: 37.55,
@@ -15,6 +15,7 @@ function Map() {
       mapStyle="mapbox://styles/devnilson/clczey9nj000s14nwoawtgygs"
       mapboxAccessToken={process.env.MAPBOX_KEY}
       {...viewport}
+      onMove={(evt) => setViewport(evt.viewState)}
     ></ReactMapGL>
   );
 }
