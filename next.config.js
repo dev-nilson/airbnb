db5cdffa84fc501525f04743c90223bd04d314bb
Denilson <dlemus@harding.edu>
2023-01-16 17:30:57 -0600

    🔐 Add mapbox key

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    MAPBOX_KEY:
+      "pk.eyJ1IjoiZGV2bmlsc29uIiwiYSI6ImNsY3plcGg3YzBmbm8zcG1vbDdnNmJ3YmQifQ.WoxkWA4QN1nvsPvi3L6HCA",
+  },
   reactStrictMode: true,
 };
