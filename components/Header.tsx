2ca3fb99051b24fc456591b04a3dc090ef1b945c
Denilson <dlemus@harding.edu>
2023-01-15 22:54:33 -0600

    🚸 Add search on submit functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -62,7 +62,13 @@ function Header({ placeholder }: HeaderProps) {
           onClick={() => router.push("/")}
         />
       </div>
-      <div className="flex items-center md:border-2 rounded-full py-2">
+      <form
+        className="flex items-center md:border-2 rounded-full py-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          search();
+        }}
+      >
         <input
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
@@ -71,7 +77,7 @@ function Header({ placeholder }: HeaderProps) {
           onChange={(e) => setSearchInput(e.target.value)}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-      </div>
+      </form>
       <div className="flex items-center space-x-4 justify-end text-gray-500">
         <p className="hidden md:inline">Become a host</p>
         <GlobeAltIcon className="h-6" />
